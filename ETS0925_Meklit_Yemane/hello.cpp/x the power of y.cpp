8c5567afaaf7445e73f6cb946b074ecbeb8c870e
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-11-25 12:07:19 +0300

    Create x the power of y.cpp

---
+++ ETS0925_Meklit_Yemane/hello.cpp/x the power of y.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int x,y,power;
+    cout <<"Enter x: ";
+    cin>>x;
+    cout <<"Enter y: ";
+    cin>>y;
+    power=pow(x,y);
+    cout<<"x the power of y is: "<<power;
+    return 0;
+}
