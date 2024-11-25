ab4e6f130ca47d60554326a128ba36cf4cd7f12a
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-11-25 12:06:10 +0300

    Update BMI.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/BMI.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/BMI.cpp
@@ -1,1 +1,20 @@
-///nh
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout <<"Enter the number of person you want to calculate: ";
+    cin >> num;
+    for(int i=1;i<=num;i++){
+    double height;
+    cout <<"Enter your height in m: ";
+    cin>>height;
+    double weight;
+    cout <<"Enter your weight in kg: ";
+    cin>>weight;
+    double BMI;
+    BMI= weight/(height * height);
+    cout <<"Your body mass index is : "<<BMI<<endl;
+     }
+ 
+        return 0;
+}
