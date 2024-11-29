50bd5cccefa4bef4e4a77d5f761e18a9300e1246
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 19:47:41 +0300

    Update Gallon.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/Gallon.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/Gallon.cpp
@@ -6,15 +6,17 @@ without refueling. */
 using namespace std;
 
 int main() {
-    double tank_capacity, miles_per_Gallon, miles_to_be_driven;
+    double tank_capacity, miles_per_Gallon, miles_to_be_driven; // Declare the variables 
 
-    cout << "Enter the capacity of an automobile fuel tank in gallons: ";
-    cin >> tank_capacity;
+    cout << "Enter the capacity of an automobile fuel tank in gallons: ";  
+    cin >> tank_capacity;   // The capacity of the automobile fuel tank is entered 
     cout << "Enter the miles per gallon of the automobile can be driven: ";
-    cin >> miles_per_Gallon;
-
-    miles_to_be_driven = tank_capacity * miles_per_Gallon;
-    cout << "The automobile can be driven " << miles_to_be_driven << " miles without refueling." << endl;
-
+    cin >> miles_per_Gallon;   // the miles that can be driven per gallon is entered 
+         if (cin.fail() ||  miles_per_Gallon <= 0 || tank_capacity < 0) {  // checks the input is valid or not
+         cout << "invalid input";
+         }else{
+    miles_to_be_driven = tank_capacity * miles_per_Gallon;   // miles to be driven is calculated 
+    cout << "The automobile can be driven " << miles_to_be_driven << " miles without refueling." << endl;  // print the output
+}
     return 0;
 }
