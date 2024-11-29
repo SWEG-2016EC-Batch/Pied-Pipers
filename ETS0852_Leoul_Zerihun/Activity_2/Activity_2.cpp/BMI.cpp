d9bddefd14c2b1b21e7f64c21f6d65f12135ef5d
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 17:03:02 +0300

    Update BMI.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/BMI.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/BMI.cpp
@@ -9,26 +9,24 @@ int main()
   float BMI; // Body Mass Index
   int number ;
   do {
-  cout << "Enter your height in meter: "; // Enter your height here
-  cin >> height;
-  cout << "Enter your weight in kilogram: "; // Enter your weight here
-  cin >> weight;
-  BMI = weight / (height * height) ; // Body Mass Index is calculated as the division of weight to height squared
-    cout << "your BMI (Body Mass Index) is: " << BMI << endl;
-        if (BMI < 18.5) {
+        cout << "Enter your height in meter: "; // Enter your height here
+        cin >> height;
+        cout << "Enter your weight in kilogram: "; // Enter your weight here
+        cin >> weight;
+        BMI = weight / (height * height) ; // Body Mass Index is calculated as the division of weight to height squared
+              cout << "your BMI (Body Mass Index) is: " << BMI << endl;
+          if (BMI < 18.5) {
               cout << "you are Underweight" << endl;
-       } else if (BMI >= 18.5 && BMI<= 24.9){
+          } else if (BMI >= 18.5 && BMI<= 24.9){
               cout << "you are normal weight" << endl;
-       }else {
+          }else {
               cout  << "you are over weight " << endl;
-   }
-  cout << "If you want to continue, enter a non-zero number if not, enter zero : " << endl;
-  cin >> number;
+          }
+            cout << "If you want to continue, enter a non-zero number if not, enter zero : " << endl;
+            cin >> number;
     
-    }while (number !=0);
-
+       }while (number !=0);
     
-    
-    
+   
     return 0;
     }
