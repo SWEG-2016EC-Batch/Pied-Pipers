7684816be8ce4f6077ea286bac76a408b3266aef
Lalisa Tamene <lalisa.tamene@aastustudent.edu.et>
2024-11-29 16:27:19 +0300

    Create BMI.cpp

---
+++ ETS0847_Lalisa_Tamene/BMI.cpp
@@ -0,0 +1,9 @@
+\*Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
+work for multiple person before terminated.*/
+
+#include <iostream>
+using namespace std;
+inr main(){
+double weight,height,bmi;
+}
