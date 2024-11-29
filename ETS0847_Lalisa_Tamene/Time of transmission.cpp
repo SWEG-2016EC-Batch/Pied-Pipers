ee0b504974b559170f31269ec729d1f0a861e4a9
Lalisa Tamene <lalisa.tamene@aastustudent.edu.et>
2024-11-29 23:26:15 +0300

    Update and rename Time of transmision.cpp to Time of transmission.cpp

--- ETS0847_Lalisa_Tamene/Time of transmission.cpp
+++ ETS0847_Lalisa_Tamene/Time of transmission.cpp
@@ -1,3 +1,7 @@
+/* Write a program that will calculate how long it will take to send a file, given the file size. Test your program on a 400MB (419,430,400 byte) file which may
+take days.A serial transmission line can transmit 960 characters a second.*/
+
+
 #include <iostream>
 #include <iomanip>
 #include <cmath> 
