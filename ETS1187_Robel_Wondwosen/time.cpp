bc98d35b8225543c7bc78012c84f9affcaf8e36e
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2024-11-29 22:25:07 +0300

    Update time.cpp

--- ETS1187_Robel_Wondwosen/time.cpp
+++ ETS1187_Robel_Wondwosen/time.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
+  int byte,time;
+    const int bytePerSecond = 960;
+    cout <<"Enter the size of your file: ";
+    cin>>byte;
+    time=byte/bytePerSecond;
+    int hours, minutes, seconds, days;
+days = time /86400;    
+time= time % 86400;
+hours = time /3600;
+time= time % 3600;
+minutes = time / 60;
+seconds = time % 60;
+    cout<<"The time it takes to send a file is: "<<days<< " days " << hours<<" hrs "<< minutes<< " mins "<< seconds<<" Seconds";
 
 
 
