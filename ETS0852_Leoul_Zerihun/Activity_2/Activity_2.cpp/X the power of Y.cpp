93dc5b3dd8b44a81bbf74105d4b73fa90c255667
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 16:43:26 +0300

    Update X the power of Y.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/X the power of Y.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/X the power of Y.cpp
@@ -1,1 +1,26 @@
+/* Write a program that find the result of  𝑥 the power of y,
+where the value of X and Y are entered by the user. */
 
+#include <iostream>
+#include <cmath>  
+using namespace std;
+
+int main() {
+  
+    double x, y, result;
+  
+    cout << "Enter the value of x: " << endl;
+    cin >> x;
+
+    cout << "Enter the value of y: " << endl;
+    cin >> y;
+if (cin.fail() ) {
+  cout << "invalid input";}
+  else {
+    result = pow(x, y);
+
+    cout << "Result of " << x << " the power of " << y << " is: " << result << endl;
+    }
+
+    return 0;
+}
