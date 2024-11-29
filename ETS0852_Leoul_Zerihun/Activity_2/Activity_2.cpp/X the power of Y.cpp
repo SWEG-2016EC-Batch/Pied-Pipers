e10a36bae37b0dc74968db992a247389eaf021a3
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 22:58:22 +0300

    Update X the power of Y.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/X the power of Y.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/X the power of Y.cpp
@@ -6,20 +6,20 @@ using namespace std;
 
 int main() {
   
-    double x, y, result;  // Declares the variabel
+    double x, y, result;  // Declare the variable
   
     cout << "Enter the value of x: " << endl;  
     cin >> x;  // enter the value of x
 
     cout << "Enter the value of y: " << endl;
     cin >> y;   // enter the value of y
-if (cin.fail() || (x==0 && y==0) ) {   // checks the validity of the input
+if (cin.fail() || (x==0 && y==0) ) {   // check the validity of the input
   cout << "invalid input";}
   
   else {
-    result = pow(x, y);  // calculates  x to power  of y 
+    result = pow(x, y);  // calculate  x to power  of y 
 
-    cout << "Result of " << x << " the power of " << y << " is: " << result << endl; // prints the result
+    cout << x << " the power of " << y << " is: " << result << endl; // print the result
     }
 
     return 0;
