cd106f021e7956beaec8e095c2d1b3c6f3f6ddd5
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2024-11-29 15:57:10 +0300

    Update miles.cpp

--- ETS1187_Robel_Wondwosen/miles.cpp
+++ ETS1187_Robel_Wondwosen/miles.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
-
-
-
+int main()
+ double gallonCapacity, mile_per_gallon, miles;
+ cout << "Enter fuel thank capacity in gallons: ";
+    cin>> gallonCapacity;
+    cout << "Enter the mile per gallon: ";
+    cin>> mile_per_gallon;
+    miles = mile_per_gallon * gallonCapacity;
+    cout << "You can travel "<< miles<< " miles without refueling. ";
 
   return 0;
 }
