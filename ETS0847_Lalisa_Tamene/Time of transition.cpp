bcc9ba8f31602b8d182adc2f7400cf91ab40af15
Lalisa Tamene <lalisa.tamene@aastustudent.edu.et>
2024-11-29 22:32:10 +0300

    Update Time of transition.cpp

--- ETS0847_Lalisa_Tamene/Time of transition.cpp
+++ ETS0847_Lalisa_Tamene/Time of transition.cpp
@@ -8,18 +8,18 @@ int main() {
     const int transmissionRate = 960;
     const long long fileSizeInBytes = 419430400;
 
-    long long Seconds = fileSizeInBytes / transmissionRate;
+    long long totalSeconds = fileSizeInBytes / transmissionRate;
 
-    long long days = Seconds / (24 * 3600);
-    Seconds %= (24 * 3600);
+    long long days = totalSeconds / (24 * 3600);
+    totalSeconds %= (24 * 3600);
 
-    long long hours = Seconds / 3600;
-    Seconds %= 3600;
+    long long hours = totalSeconds / 3600;
+    totalSeconds %= 3600;
 
-    long long minutes = Seconds / 60;
-    Seconds %= 60;
+    long long minutes = totalSeconds / 60;
+    totalSeconds %= 60;
 
-    long long seconds = Seconds;
+    long long totalseconds = totalSeconds;
 
     cout << "File size: " << fileSizeInBytes << " bytes" << endl;
     cout << "Transmission rate: " << transmissionRate << " characters per second" << endl;
@@ -27,7 +27,7 @@ int main() {
          << days << " days, "
          << hours << " hours, "
          << minutes << " minutes, "
-         << seconds << " seconds." << endl;
+         << totalseconds << " seconds." << endl;
 
     return 0;
 }
