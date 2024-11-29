2d5f102d553714fdd3b167fea122511a5426d485
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-11-29 17:04:56 +0300

    Update Power.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/Power.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/Power.cpp
@@ -2,12 +2,12 @@
 #include<cmath>
 using namespace std;
 int main(){
-    int x,y,power;
-    cout <<"Enter x: ";
+    int x,y,result;
+    cout <<"Enter the base: ";
     cin>>x;
-    cout <<"Enter y: ";
+    cout <<"Enter : ";
     cin>>y;
-    power=pow(x,y);
-    cout<<"x the power of y is: "<<power;
+    result=pow(x,y);
+    cout << "The result of " << x << " raised to the power " << y << " is: " << result << endl;
     return 0;
 }
