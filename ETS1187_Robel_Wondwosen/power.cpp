b1655c4c1181de4c369150d5e5c8abaa6d610ff7
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2024-11-29 18:49:47 +0300

    Update power.cpp

--- ETS1187_Robel_Wondwosen/power.cpp
+++ ETS1187_Robel_Wondwosen/power.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
+  
+   int x,y,power;
+    cout<< "Enter X: ";
+    cin >> x;
+    cout<< "Enter y: ";
+    cin >> y;
+    power= pow(x, y);
+    cout << "X the power of Y is: "<< power;
 
-
-
+  
   return 0;
 }
