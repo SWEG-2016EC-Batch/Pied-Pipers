c8bfa836c5aba4c65cd0ca458c163b12f4e91418
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-11-29 16:52:19 +0300

    Update BMI.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/BMI.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/BMI.cpp
@@ -1,20 +1,43 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int num;
-    cout <<"Enter the number of person you want to calculate: ";
-    cin >> num;
-    for(int i=1;i<=num;i++){
-    double height;
-    cout <<"Enter your height in m: ";
-    cin>>height;
-    double weight;
-    cout <<"Enter your weight in kg: ";
-    cin>>weight;
-    double BMI;
-    BMI= weight/(height * height);
-    cout <<"Your body mass index is : "<<BMI<<endl;
-     }
+#include <iostream>
+ #include <iomanip>
+ using namespace std;
+ int main(){
+    /*BMI calculator 
+    weight(kg) / height * height (m)
+    Underweight: BMI<18.5
+    Normalweight: 18.5<= BMI<= 24.9
+    Overweight: BMI >25
+    */
+   char choice;
+   do{
+   double height,weight,bmi;
+   cout <<"Enter your height in m: ";
+   cin>>height;
+   cout <<"Enter your weight in kg: ";
+   cin>>weight;
+   if(height <= 0){
+    cout <<"Invalid height. Please enter a positive value.\n";
+    continue;
+   }
+   bmi=weight/(height * height);
+
+    cout <<"Your body mass index is: "<<bmi<<endl;
+     if (bmi < 18.5) { 
+        cout <<"You are Underweight"<<endl;
+         } 
+         else if (bmi >= 18.5 && bmi <= 24.9) 
+         {
+             cout <<"You are Normalweight"<<endl; 
+         } 
+         else { 
+            cout <<"You are overweight"<<endl; 
+            }
+      cout << "Do you want to calculate BMI for another person? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Program terminated. Goodbye!\n";
  
-        return 0;
-}
+   }
+    return 0;
+ }
