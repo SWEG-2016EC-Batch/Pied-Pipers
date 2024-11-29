a9ffab84a4e31e791ccb90ac9ae5d49e61979d9f
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 22:27:46 +0300

    Update File_transfer.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/File_transfer.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/File_transfer.cpp
@@ -14,12 +14,12 @@ int main(){
     cout << "invalid input"; 
     }else{
     byte = 1048576 * file_size ;   //converts the Mb file to bytes
-    time=byte/sending_rate;   // calculates the time it takes to send the file
+    time =byte/sending_rate;   // calculates the time it takes to send the file
     // convert the time to days, hours,minutes and seconds;
-days= time /86400;    
-time= time % 86400;
+days = time /86400;    
+time = time % 86400;
 hours = time /3600;
-time= time % 3600;
+time = time % 3600;
 minutes = time / 60;
 seconds = time % 60;
     cout<<"The time it takes to send " << file_size << " Mb is: "<<endl<< days<< " days " <<endl << hours<<" hrs " <<endl << minutes<< " mins "<<endl<< seconds<<" Seconds";
