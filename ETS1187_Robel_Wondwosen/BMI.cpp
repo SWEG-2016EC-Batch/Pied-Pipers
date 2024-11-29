00f94c423163f17c099cd3f82de0fafb5b7a8c42
Robel Wondwosen <121330659+Robel-w@users.noreply.github.com>
2024-11-29 13:37:56 +0300

    Update BMI.cpp

--- ETS1187_Robel_Wondwosen/BMI.cpp
+++ ETS1187_Robel_Wondwosen/BMI.cpp
@@ -1,19 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-    int num;
-    cout <<"Enter the number of person you want to calculate: ";
-    cin >> num;
-    for(int i=1;i<=num;i++){
-    double height;
-    cout <<"Enter your height in M: ";
-    cin>>height;
-    double weight;
-    cout <<"Enter your weight in kg: ";
-    cin>>weight;
-    double BMI;
-    BMI= weight/(height * height);
-    cout <<"Your body mass index is : "<<BMI<<endl;
- 
-        return 0;
+
+int main() {
+    while (true) {
+        double weight, height, bmi;
+        cout << "Enter weight (in kg): ";
+        cin >> weight;
+        cout << "Enter height (in meters): ";
+        cin >> height;
+        if (cin.fail() || height <= 0) {
+            cout << "Invalid input! Program terminated.\n";
+            break;
+        }
+        bmi = weight / (height * height);
+        cout << "Your BMI is: " << bmi << endl;
+        if (bmi < 18.5)
+            cout << "you are UnderWeight\n";
+        else if (bmi >= 18.5 && bmi < 24.9)
+            cout << "You are NormalWeight\n";
+        else if (bmi >= 25.0 && bmi <=29.5)
+            cout << "You are OverWeight\n";
+        else
+            cout <<"you are Obese\n";
+
+      
+    }
+
+    return 0;
 }
+
