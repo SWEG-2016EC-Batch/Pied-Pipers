b1c74917a75a2884133502914919b4f781af49c8
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-11-29 19:38:39 +0300

    Update BMI.cpp

--- ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/BMI.cpp
+++ ETS0852_Leoul_Zerihun/Activity_2/Activity_2.cpp/BMI.cpp
@@ -1,3 +1,6 @@
+/* Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+in normal weight, underweight or overweight. BMI is calculated as (BMI = w/h*h). Make the program to
+work for multiple person before terminated. */
 
 #include<iostream>
 using namespace std;
@@ -8,24 +11,31 @@ int main()
   float height;
   float BMI; // Body Mass Index
   int number ;
-  do {
-        cout << "Enter your height in meter: "; // Enter your height here
+  cout << " For how many person do you want to calculate BMI (Body Mass Index): "; 
+  cin >> number;
+   
+       for (int i = 1; i <= number ; i++) {      // iterates untill the number entered is reached
+        cout << "Enter the height in meter: " << endl; // Enter your height here
         cin >> height;
-        cout << "Enter your weight in kilogram: "; // Enter your weight here
+        cout << "Enter the weight in kilogram: " << endl; // Enter your weight here
         cin >> weight;
+   if (cin.fail() || weight <= 0 || height <= 0) {   // checks whether the input is valid or not
+   cout << "invalid input" << endl;
+   break;  
+   }else{
+   
         BMI = weight / (height * height) ; // Body Mass Index is calculated as the division of weight to height squared
               cout << "your BMI (Body Mass Index) is: " << BMI << endl;
-          if (BMI < 18.5) {
-              cout << "you are Underweight" << endl;
-          } else if (BMI >= 18.5 && BMI<= 24.9){
-              cout << "you are normal weight" << endl;
+          if (BMI < 18.5) {         // checks if the BMI is lessthan 18.5
+              cout << "you are Underweight" << endl;    // prints  underweight if BMI is lessthan 18.5
+          } else if (BMI >= 18.5 && BMI<= 24.9){       // checks if the BMI is between 18.5 and 24.9
+              cout << "you are normal weight" << endl;   // prints normalweight if BMI is between the range
           }else {
-              cout  << "you are over weight " << endl;
+              cout  << "you are Overweight " << endl;  // prints overweight if BMI is greaterthan 25
           }
-            cout << "If you want to continue, enter a non-zero number if not, enter zero : " << endl;
-            cin >> number;
-    
-       }while (number !=0);
+            
+       }
+                  }
     
    
     return 0;
