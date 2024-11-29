684d8cd47238f7e093faca9a8099309c028b61d3
Lalisa Tamene <lalisa.tamene@aastustudent.edu.et>
2024-11-29 23:27:49 +0300

    Update Number of mile.cpp

--- ETS0847_Lalisa_Tamene/Number of mile.cpp
+++ ETS0847_Lalisa_Tamene/Number of mile.cpp
@@ -1,3 +1,7 @@
+/* Write a program that prompts the capacity in gallons of an automobile fuel tank and the miles per gallons
+the automobiles can be driven. The program outputs the number miles the automobile cam be driven
+without refueling.*/
+
 #include <iostream>
 using namespace std;
 
