14898e550a80e7b9c655064b673e8c8c0ab080b4
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-12-11 02:24:14 +0300

    Update count.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/count.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/count.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+  
+    int number, count = 0;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    
+    if (number == 0) {
+        count = 1;
+    } else {
+      
+        number = abs(number); 
+        while (number != 0) {
+            number /= 10; 
+            count++;
+        }
+    }
+
+    cout << "Number of digits: " << count << endl;
+
+    return 0;
+}
 
