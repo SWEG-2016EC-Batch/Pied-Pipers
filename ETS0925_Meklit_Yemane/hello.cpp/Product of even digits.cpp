a8c869b68de5810d8a162746cc8ec25cb8efb41d
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-12-11 02:42:36 +0300

    Update Product of even digits.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/Product of even digits.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/Product of even digits.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main() {
+    
+    int number, digit;
+    int product = 1; 
+    bool hasEvenDigit = false; 
+
+   
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    
+    number = abs(number);
+
+   
+    while (number > 0) {
+        digit = number % 10; 
+
+        
+        if (digit % 2 == 0) {
+            product *= digit; 
+            hasEvenDigit = true; 
+        }
+
+        number /= 10; 
+    }
+
+    
+    if (hasEvenDigit) {
+        cout << "Product of even digits: " << product << endl;
+    } else {
+        cout << "No even digits found." << endl;
+    }
+
+    return 0;
+}
 
