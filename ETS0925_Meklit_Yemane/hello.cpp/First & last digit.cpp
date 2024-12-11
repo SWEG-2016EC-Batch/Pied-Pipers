a020af5a7dc025a8ea579b9b645f003b3cdb4ca6
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-12-11 03:29:31 +0300

    Update First & last digit.cpp

--- ETS0925_Meklit_Yemane/hello.cpp/First & last digit.cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/First & last digit.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, firstDigit, lastDigit, sum;
+
+    
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    
+    lastDigit = abs(number) % 10;
+
+    firstDigit = abs(number);
+    while (firstDigit >= 10) {
+        firstDigit /= 10;
+    }
+
+   
+    sum = firstDigit + lastDigit;
+
+    
+    cout << "First digit: " << firstDigit << endl;
+    cout << "Last digit: " << lastDigit << endl;
+    cout << "Sum of first and last digit: " << sum << endl;
+
+    return 0;
+}
 
