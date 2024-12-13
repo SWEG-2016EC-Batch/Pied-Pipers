f11c3901613318572eb85b2763ffe1198777df1e
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2024-12-13 23:00:22 +0300

    Update Playing_with_numbers.cpp

--- ETS0852_Leoul_Zerihun/Activity_3/Activity_3.cpp/Playing_with_numbers.cpp
+++ ETS0852_Leoul_Zerihun/Activity_3/Activity_3.cpp/Playing_with_numbers.cpp
@@ -3,65 +3,126 @@
 using namespace std;
 
 int main() {
-    int choice, n, original, reverse, count, product, first, last, sum;
-    int num_digits, swapped, temp, sum_fact, sum_divisors;
+    int choice, num,original, reverse, count, product, first, last, sum;
+    int num_digits, digit, swapped, temp, sum_arm, sum_divisors;
     int freq[10];
 
     
-        cout << "Choose a functionality:\n1. Reverse the number\n2. Count digits\n3. Product of even digits\n4. First and last digit sum\n5. Swap first and last digit\n6. Check palindrome\n7. Digit frequency\n8. Check Strong number\n9. Check Perfect number\n0. Exit\nEnter your choice: ";
-        cin >> choice;
+      cout << "Choose (0-9): " << endl;
+      cout <<"1. Reverse the number" <<endl;
+      cout << "2. Count digits" <<endl;
+      cout << "3. Product of even digits"<<endl;
+      cout << "4. First and last digit sum"<<endl;
+      cout << "5. Swap first and last digit"<<endl;
+       cout << "6. Check palindrome"<<endl;
+       cout << "7. Digit frequency"<<endl;
+       cout <<"8. Check Armstrong number"<<endl;
+      cout << "9. Check Perfect number"<<endl;
+      cout << "0. Exit"<<endl;
+       cout << "Enter your choice: ";
+      cin >> choice;
 
-        if (choice == 0) break;
+        if (choice == 0) {
+        return 1;}
         
-        cout << "Enter an integer: ";
-        cin >> n;
-        original = n;
+        cout << "Enter a number: ";
+        cin >> num;
+        original = num;
         reverse = 0; count = 0; product = 1; sum = 0;
         for (int i = 0; i < 10; i++) freq[i] = 0;
 
         switch(choice) {
             case 1:
-                while (n != 0) { last = n % 10; reverse = reverse * 10 + last; n /= 10; }
+                while (num != 0) { 
+                last = num % 10; 
+                reverse = reverse * 10 + last; 
+                num /= 10; 
+                }
                 cout << "Reverse: " << reverse << endl;
                 break;
             case 2:
-                while (n != 0) { n /= 10; count++; }
+                while (num != 0) { 
+                num /= 10; count++;
+                 }
                 cout << "Number of digits: " << count << endl;
                 break;
             case 3:
-                while (n != 0) { last = n % 10; if (last % 2 == 0) product *= last; n /= 10; }
+                while (num != 0) {
+                 last = num % 10;
+                  if (last % 2 == 0) {
+                  product *= last; num /= 10; 
+                 }
+                 }
                 cout << "Product of even digits: " << product << endl;
                 break;
             case 4:
-                first = original; while (first >= 10) first /= 10; last = original % 10;
+                first = original;
+                 while (first >= 10) {
+                 first /= 10; last = original % 10;
+                }
                 cout << "Sum of first and last digit: " << first + last << endl;
                 break;
             case 5:
-                first = original; while (first >= 10) first /= 10; last = original % 10;
-                num_digits = log10(original); swapped = last * pow(10, num_digits) + (original % int(pow(10, num_digits))) / 10 * 10 + first;
+                first = original; 
+                while (first >= 10) {
+                first /= 10;
+                 last = original % 10;
+                num_digits = log10(original);
+                 swapped = last * pow(10, num_digits) + (original % int(pow(10, num_digits))) / 10 * 10 + first;
+                }
                 cout << "After swapping: " << swapped << endl;
                 break;
             case 6:
                 temp = original; reverse = 0;
-                while (temp != 0) { last = temp % 10; reverse = reverse * 10 + last; temp /= 10; }
-                cout << (reverse == original ? "Palindrome" : "Not a palindrome") << endl;
-                break;
+                while (temp != 0) {
+                last = temp % 10; 
+                reverse = reverse * 10 + last; 
+                temp /= 10; }
+                if (reverse == original) {
+                cout << "Is palindrome";
+                }else{
+                cout << "Isn't Palindrome";
+                }            
+             break;
             case 7:
-                n = original;
-                while (n != 0) { last = n % 10; freq[last]++; n /= 10; }
-                for (int i = 0; i < 10; i++) if (freq[i] > 0) cout << i << ": " << freq[i] << endl;
+                num = original;
+                while (num != 0) { 
+                last = num% 10; 
+                freq[last]++; 
+                num /= 10; 
+                }for (int i = 0; i < 10; i++) {
+                if (freq[i] > 0) 
+                {cout << i << ": " << freq[i] << endl;
+                }
+                }
                 break;
             case 8:
-                sum_fact = 0; n = original;
-                while (n != 0) { int digit = n % 10; int fact = 1; for (int i = 1; i <= digit; i++) fact *= i; sum_fact += fact; n /= 10; }
-                cout << (sum_fact == original ? "Strong number" : "Not a strong number") << endl;
+                sum_arm = 0; num = original;
+                while (num != 0) { 
+                digit = num % 10;
+                  sum_arm += digit*digit*digit; 
+                  num /= 10; 
+                  }
+                if (sum_arm == original ) {
+                cout << "Is Armstrong"<< endl;
+                }else {
+                cout << "Isn't Armstrong"<< endl;
+                }
                 break;
-            case 9:
+         case 9:
                 sum_divisors = 0;
-                for (int i = 1; i < original; i++) if (original % i == 0) sum_divisors += i;
-                cout << (sum_divisors == original ? "Perfect number" : "Not a perfect number") << endl;
+                for (int i = 1; i < original; i++){
+                if (original % i == 0) {
+                sum_divisors += i;
+                }
+                if (sum_divisors == original) {
+                cout << "Perfect number";
+                }else{
+                cout << "Not perfect number";
+                }
+                }
                 break;
-            default:
+        default:
                 cout << "Invalid choice!" << endl;
         }
         cout << endl;
