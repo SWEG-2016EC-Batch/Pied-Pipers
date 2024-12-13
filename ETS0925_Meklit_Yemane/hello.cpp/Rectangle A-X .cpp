5bb6aaa5144a9b8244a05ea7fec4d229f64943ca
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2024-12-13 03:57:43 +0300

    Update Rectangle A-X .cpp

--- ETS0925_Meklit_Yemane/hello.cpp/Rectangle A-X .cpp
+++ ETS0925_Meklit_Yemane/hello.cpp/Rectangle A-X .cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    char letter = 'A'; 
+
+    cout << endl;
+
+    
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            cout << letter << " "; 
+            letter++;
+            if (letter > 'X') {
+                letter = 'A';
+            }
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
 
