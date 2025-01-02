598ba05c1684a3a787c4ea873e9827450ef2c3a0
Leoul Zerihun <leoul.zerihun@aastustudent.edu.et>
2025-01-02 11:50:26 +0300

    Update Test_Marker.cpp

--- Test_Marker/Test_Marker.cpp
+++ Test_Marker/Test_Marker.cpp
@@ -56,7 +56,87 @@ for (int i = 0; i < num_students; ++i) {
             }
         }
     }
+    do {
+        // Switch cases for functionalities
+        cout << endl << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+        cout << "Which functionality do you want: " << endl;
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+        cout << "\n1. See top 5 or top 10 students " << "\n2. Search for a student " << endl;
+        cout<<"~~~~> ";
+        cin >> functionality; 
+          
 
+        switch (functionality) {
+            case 1: {
+                // Display top students based on user choice
+                cout << "Enter 5 or 10: ";
+                cin >> choice;
+
+                if (choice != 5 && choice != 10) {
+                    cout << "Invalid choice. Please restart the program and enter either 5 or 10." << endl;
+                    return 1;
+                }
+
+                // Display top students
+                cout << "\nTop " << choice << " Students:" << endl;
+                for (int i = 0; i < choice && i < num_students; ++i) {
+                    cout << i + 1 << ". " << "Student ID: " << students[i][0] << " - " << total_scores[i] << " marks - Grade: " << grades[i] << endl;
+                }
+                break;
+            }
+
+            case 2: {
+                // Search for student by ID and display their information
+                cout << "Enter the ID of the student you want to check: "; 
+                string student_id; 
+                cin >> student_id;
+                bool student_found = false; 
+                for (int i = 0; i < num_students; i++) { 
+                    if (students[i][0] == student_id) { 
+                        student_found = true; 
+                        cout << "- - - - - - - - - -"<< endl 
+                             << "\nStudent ID: " << student_id 
+                             << "\nTotal Score: " << total_scores[i] 
+                             << "\nAnswers: "; 
+                        for (int j = 1; j <= 20; j++) { 
+                            cout << j<<"."<<students[i][j] << " ";
+                        } 
+                        cout << "\nGrade: " << grades[i] << endl << "- - - - - - - - - - " << endl; 
+                        break;
+                    }
+                }
+                if (!student_found) { 
+                    cout << "Student ID: " << student_id << " not found." << endl; 
+                }
+                break;
+            }
+
+            default: 
+                cout << "Invalid functionality. Please try again." << endl;
+        }
+
+        cout << endl<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<< endl 
+                    << "Do you want to continue? If yes, enter Y "<< endl 
+                    <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+        
+        cout<<"~~~~~> ";
+        
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y');  
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < num_students; ++i) {
+        delete[] students[i];
+    }
+    delete[] students;
+    delete[] total_scores;
+    delete[] grades;
+
+    cout << "\n********************************" << endl;
+    cout << "#          THANK YOU!           #" << endl;
+    cout << "********************************" << endl;
+}
 
 
   return 0;
