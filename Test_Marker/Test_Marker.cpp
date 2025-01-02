61255b57f3ba0a2380b6c95e1aa0b4723ead0421
Meklit Yemane <184155273+Meklit196@users.noreply.github.com>
2025-01-02 11:32:06 +0300

    Update Test_Marker.cpp

--- Test_Marker/Test_Marker.cpp
+++ Test_Marker/Test_Marker.cpp
@@ -1,7 +1,39 @@
 #include<iostream>
 #include <ctype.h>
 using namespace std;
+// Scoring and Grading
+int* total_scores = new int[num_students];
+char* grades = new char[num_students];
+string correct_answers[20] = {"TRUE", "TRUE", "FALSE", "TRUE", "FALSE",
+                              "A", "B", "C", "D", "A",
+                              "B", "C", "D", "A", "B",
+                              "VERB TO BE", "WILLIAM SHAKESPEARE", "VERB", "SPEAKING", "AMERICA"};
 
+for (int i = 0; i < num_students; ++i) {
+    int total_score = 0;
+    for (int j = 1; j <= 20; ++j) {
+        if (students[i][j] == correct_answers[j - 1]) {
+            total_score += 2;
+        } else if (students[i][j].empty()) {
+            total_score += 0;
+        } else {
+            total_score -= 1;
+        }
+    }
+    total_scores[i] = total_score;
+    float percentage = total_score * 2.5;
+    if (percentage >= 90 && percentage <= 100) {
+        grades[i] = 'A';
+    } else if (percentage >= 80 && percentage < 90) {
+        grades[i] = 'B';
+    } else if (percentage >= 70 && percentage < 80) {
+        grades[i] = 'C';
+    } else if (percentage >= 60 && percentage < 70) {
+        grades[i] = 'D';
+    } else {
+        grades[i] = 'F';
+    }
+}
 int main() {    
      
  return 0;
